d39de5bd0b164c404a218aa5404295015b26bfe4
bmuschko <benjamin.muschko@gmail.com>
2011-10-09 19:56:11 -0400

    Added JavaDoc.

--- src/main/groovy/org/gradle/api/plugins/clover/CloverReportTask.groovy
+++ src/main/groovy/org/gradle/api/plugins/clover/CloverReportTask.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gradle.api.plugins.clover
 
 import org.gradle.api.DefaultTask
@@ -7,11 +22,9 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 /**
- * Created by IntelliJ IDEA.
- * User: Ben
- * Date: 9/24/11
- * Time: 7:30 PM
- * To change this template use File | Settings | File Templates.
+ * Base class for Clover report tasks.
+ *
+ * @author Benjamin Muschko
  */
 class CloverReportTask extends DefaultTask {
     private static final Logger LOGGER = LoggerFactory.getLogger(CloverReportTask)
