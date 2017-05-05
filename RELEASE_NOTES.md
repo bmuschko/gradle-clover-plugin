4dd7cd9e8f84fd49e52e08e2485983c57cc209eb
Alex Volanis <alexander.volanis@simplivity.com>
2017-05-04 22:09:36 -0400

    Add integration tests for PR#78
    Updated RELEASE_NOTES.md with fix info.

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -3,6 +3,7 @@
 * Fix Java project with groovy tests can't find groovyc - [Issue 59](https://github.com/bmuschko/gradle-clover-plugin/issues/37).
 * Fix License issues in multi-project config - [Issue 66](https://github.com/bmuschko/gradle-clover-plugin/issues/66).
 * Fix Support instrumentLambda Clover property - [Issue 62](https://github.com/bmuschko/gradle-clover-plugin/issues/62).
+* Fix additionalTestDirs adds sourceSet to test sourceSet - [Issue 11](https://github.com/bmuschko/gradle-clover-plugin/issues/11).
 
 ### Version 2.0.1 (November 15, 2014)
 
--- src/integTest/groovy/com/bmuschko/gradle/clover/CloverPluginIntegSpec.groovy
+++ src/integTest/groovy/com/bmuschko/gradle/clover/CloverPluginIntegSpec.groovy
@@ -48,6 +48,7 @@ class CloverPluginIntegSpec extends Specification {
         coverage.project.metrics.@classes == '1'
         coverage.project.metrics.@methods == '2'
         coverage.project.metrics.@coveredmethods == '1'
+        coverage.testproject.metrics.@files == '1'
         coverage.testproject.metrics.@classes == '1'
         coverage.testproject.metrics.@methods == '1'
         coverage.testproject.metrics.@coveredmethods == '1'
@@ -67,6 +68,31 @@ class CloverPluginIntegSpec extends Specification {
 
         then: "the Clover reports are generated again"
         cloverXmlReport.exists()
+        cloverHtmlReport.exists()
+        cloverJsonReport.exists()
+        cloverPdfReport.exists()
+    }
+
+    def "Build a Java project with additional tests"() {
+        given: "a Java project with additional tests"
+        projectName = 'java-project-with-additional-tests'
+
+        when: "the Clover report generation task is run"
+        runTasks('clean', 'cloverGenerateReport')
+
+        then: "the Clover coverage database is generated"
+        cloverDb.exists()
+
+        and: "the Clover report is generated and is correct"
+        cloverXmlReport.exists()
+        def coverage = new XmlSlurper().parse(cloverXmlReport)
+        coverage.project.metrics.@classes == '1'
+        coverage.project.metrics.@methods == '2'
+        coverage.project.metrics.@coveredmethods == '1'
+        coverage.testproject.metrics.@files == '2'
+        coverage.testproject.metrics.@classes == '2'
+        coverage.testproject.metrics.@methods == '2'
+        coverage.testproject.metrics.@coveredmethods == '2'
         cloverHtmlReport.exists()
         cloverJsonReport.exists()
         cloverPdfReport.exists()
